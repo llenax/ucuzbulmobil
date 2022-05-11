684e95ac41872455c07424c47e705134f4c19dee
llenax <jaimitr14@gmail.com>
2022-05-11 23:20:01 +0300

    SliderAdapter.java | Image, Text - basic implementation

--- app/src/main/java/com/ucuzbul/ucuzbulmobil/adapters/SliderAdapter.java
+++ app/src/main/java/com/ucuzbul/ucuzbulmobil/adapters/SliderAdapter.java
@@ -1,7 +1,7 @@
 package com.ucuzbul.ucuzbulmobil.adapters;
 
 import android.content.Context;
-import android.media.Image;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,22 +9,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.ucuzbul.ucuzbulmobil.R;
 
-import org.w3c.dom.Text;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class SliderAdapter extends PagerAdapter {
 
     Context context;
 
-    int[] items;
+    List<Map<Integer, String>> items;
 
     LayoutInflater layoutInflater;
 
-    public SliderAdapter(Context context, int[] items) {
+    public SliderAdapter(Context context, List<Map<Integer, String>> items) {
         this.context = context;
         this.items = items;
 
@@ -33,7 +36,7 @@ public class SliderAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return items.length;
+        return items.size();
     }
 
     @Override
@@ -41,15 +44,17 @@ public class SliderAdapter extends PagerAdapter {
         return view == ((ConstraintLayout) object);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         View itemView = layoutInflater.inflate(R.layout.slider_item, container, false);
 
         ImageView imageView = (ImageView) itemView.findViewById(R.id.slider_content_img);
-        imageView.setImageResource(items[position]);
+        imageView.setImageResource(Objects.requireNonNull(getMapPositionIndexVal(position)).getKey());
 
         TextView textView = (TextView) itemView.findViewById(R.id.slider_content_text);
+        textView.setText(Objects.requireNonNull(getMapPositionIndexVal(position)).getValue());
 
         container.addView(itemView);
 
@@ -61,4 +66,13 @@ public class SliderAdapter extends PagerAdapter {
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((ConstraintLayout) object);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Map.Entry<Integer, String> getMapPositionIndexVal(int position) {
+        if( items.get(position).entrySet().stream().findFirst().isPresent()) {
+            return items.get(position).entrySet().stream().findFirst().get();
+        }
+        return null;
+    }
+
 }
